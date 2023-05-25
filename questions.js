fda2e922e8efe5f55320d7700e3d3fea2e2546cc
carlopergo <128333507+carlopergo@users.noreply.github.com>
2023-05-25 01:05:42 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which bear is the biggest?",
+    answer: "Polar Bear",
+    options: [
+      "Grizzly Bear",
+      "Polar Bear",
+      "Kodiak Bear",
+      "Giant Pandas"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many species of bear are there?",
+    answer: "8",
+    options: [
+      "11",
+      "6",
+      "8",
+      "12"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which species are bears closely related too?",
+    answer: "Canines",
+    options: [
+      "Ovines",
+      "Felines",
+      "Caprines",
+      "Canines"
+    ]
+  }
+];
